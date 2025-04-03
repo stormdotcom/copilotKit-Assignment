53869124456007e2fc7295da2f7c04e6e28f2cac
Ajmal Nasumudeen <ajmaln73@gmail.com>
2025-04-03 14:56:48 +0530

    error fix

--- ui/app/api/copilotkit/route.ts
+++ ui/app/api/copilotkit/route.ts
@@ -1,37 +1,37 @@
-import {
-    CopilotRuntime,
-    ExperimentalEmptyAdapter,
-    copilotRuntimeNextJSAppRouterEndpoint,
-    langGraphPlatformEndpoint
-    // ...
-} from "@copilotkit";
-import { NextRequest } from "next/server";
+// import {
+//     CopilotRuntime,
+//     ExperimentalEmptyAdapter,
+//     copilotRuntimeNextJSAppRouterEndpoint,
+//     langGraphPlatformEndpoint
+//     // ...
+// } from "@copilotkit";
+// import { NextRequest } from "next/server";
 
-// You can use any service adapter here for multi-agent support.
-const serviceAdapter = new ExperimentalEmptyAdapter();
+// // You can use any service adapter here for multi-agent support.
+// const serviceAdapter = new ExperimentalEmptyAdapter();
 
-const runtime = new CopilotRuntime({
-    remoteEndpoints: [
-        langGraphPlatformEndpoint({
-            deploymentUrl: "your-api-url", // make sure to replace with your real deployment url,
-            langsmithApiKey: "sv2_pt_26322adbab5240d08ef73ad64556f2c3_ecf47fea0f", // only used in LangGraph Platform deployments
-            agents: [ // List any agents available under "graphs" list in your langgraph.json file; give each a description explaining when it should be called.
-                {
-                    name: 'sample_agent',
-                    description: 'A helpful LLM agent.',
-                    assistantId: 'your-assistant-ID' // optional, but recommended!
-                }
-            ]
-        }),
-    ],
-});
+// const runtime = new CopilotRuntime({
+//     remoteEndpoints: [
+//         langGraphPlatformEndpoint({
+//             deploymentUrl: "your-api-url", // make sure to replace with your real deployment url,
+//             langsmithApiKey: "sv2_pt_26322adbab5240d08ef73ad64556f2c3_ecf47fea0f", // only used in LangGraph Platform deployments
+//             agents: [ // List any agents available under "graphs" list in your langgraph.json file; give each a description explaining when it should be called.
+//                 {
+//                     name: 'sample_agent',
+//                     description: 'A helpful LLM agent.',
+//                     assistantId: 'your-assistant-ID' // optional, but recommended!
+//                 }
+//             ]
+//         }),
+//     ],
+// });
 
-export const POST = async (req: NextRequest) => {
-    const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
-        runtime,
-        serviceAdapter,
-        endpoint: "/api/copilotkit",
-    });
+// export const POST = async (req: NextRequest) => {
+//     const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
+//         runtime,
+//         serviceAdapter,
+//         endpoint: "/api/copilotkit",
+//     });
 
-    return handleRequest(req);
-};
+//     return handleRequest(req);
+// };
--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -3,12 +3,13 @@ import "@copilotkit/react-ui/styles.css";
 import { ReactNode } from "react";
 import { DocumentProvider } from "./context/DocumentContext";
 import "./globals.css";
+
 export default function RootLayout({ children }: { children: ReactNode }) {
-  // const apiUrl = process.env.NEXT_PUBLIC_COPILOT_KIT_API;
+  const apiUrl = process.env.NEXT_PUBLIC_COPILOT_KIT_API;
   return (
     <html lang="en">
       <body>
-        <CopilotKit runtimeUrl="/api/copilotkit" agent="sample_agent">
+        <CopilotKit publicApiKey={apiUrl} agent="sample_agent">
           <DocumentProvider>{children}</DocumentProvider>
         </CopilotKit>
       </body>
